6952cced0cbe64be10bc6c6b356051ca1283c928
venerari <venerayan@gmail.com>
2018-05-09 22:36:46 -0400

    22

--- test/run-1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Check extra repository, git, ansible, sshpass
-rpm -qa epel-release > epelx
-if [[ ! -s epelx ]]; then
-   sudo wget https://archive.fedoraproject.org/pub/epel/7Server/x86_64/Packages/e/epel-release-7-11.noarch.rpm
-   sudo rpm -ivh epel-release-7-11.noarch.rpm
-   sudo yum install git ansible sshpass -y
-fi
